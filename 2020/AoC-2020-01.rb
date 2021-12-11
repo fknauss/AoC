9054c1e1826e1668535a143ffbaffe8cc13a1d29
fknauss <fknauss@cultureshark.net>
2021-12-11 11:33:05 -0800

    Changed file paths Moved library

---
+++ 2020/AoC-2020-01.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+#https://adventofcode.com/2020/day/1
+
+PRODUCTION = true #&& false
+source = PRODUCTION ? open('./2020/input-01.txt') : DATA
+
+data = source.readlines(chomp:true).map(&:to_i)
+
+#part 1
+p data.combination(2).filter{|a| a.sum == 2020}.first.inject(&:*)
+p data.combination(3).filter{|a| a.sum == 2020}.first.inject(&:*)
+
+__END__
+1721
+979
+366
+299
+675
+1456
