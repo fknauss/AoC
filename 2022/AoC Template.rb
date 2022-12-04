86f7d8fc92922177dc5b5a949c991699be1f30b1
fknauss <fknauss@cultureshark.net>
2022-12-03 23:23:19 -0800

    day 4 + template

---
+++ 2022/AoC Template.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# 
+
+PRODUCTION = true #&& false
+source = PRODUCTION ? open('') : DATA
+#fetch and format
+data = source.readlines(chomp: true).
+
+#prep
+
+#part 1
+
+#part 2
+
+__END__
