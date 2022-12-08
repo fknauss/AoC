36a828c8223815b5d798b38a67220085b4aa20b5
fknauss <fknauss@cultureshark.net>
2022-12-08 05:15:44 -0800

    Unuglying

--- 2022/AoC-2022-08.rb
+++ 2022/AoC-2022-08.rb
@@ -8,25 +8,20 @@ source = PRODUCTION ? open('AoC-2022-08.txt') : DATA
 data = source.readlines(chomp: true).map{_1.chars.map(&:to_i)}
 
 def pzip(d)
-  sees(d).flatten.zip(
-  sees(d.map(&:reverse)).map(&:reverse).flatten,
-  sees(d.transpose).transpose.flatten,
-  sees(d.reverse.transpose).transpose.reverse.flatten
-  )
+  d.map{|r| yield r}.flatten.zip(
+  d.map{|r| (yield r.reverse).reverse}.flatten,
+  d.transpose.map{|r| yield r}.transpose.flatten,
+  d.reverse.transpose.map{|r| yield r}.transpose.reverse.flatten)
 end
 
 #part 1
-def sees(d)
-  d.map{|r| a=-1; r.map{b,a=a,[a,_1].max;b}.zip(r).map{|(x,y)|y>x}}
-end
-
-p pzip(data).map{_1.inject(&:|)}.select{_1}.count
+p pzip(data){|r| a=-1; r.map{b,a=a,[a,_1].max;_1>b}}.
+            map{_1.inject(&:|)}.select{_1}.count
 
 #part 2
-def sees(d) # total hack
-  d.map{|r| (0...r.size).map{|s|r.drop(s+1).chunk_while{_1<r[s]}.first&.size||0}}
-end
-p pzip(data).map{_1.inject(&:*)}.max
+p pzip(data){|r| (0...r.size).map{|s|r.drop(s+1).
+                              chunk_while{_1<r[s]}.first&.size||0}}.
+            map{_1.inject(&:*)}.max
 
 __END__
 30373
